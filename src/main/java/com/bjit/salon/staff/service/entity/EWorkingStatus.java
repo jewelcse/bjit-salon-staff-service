d2e32eca87901bdd84178fd8889b87e15d88aa8e
Jewel Chowdhury <jewel.chowdhury@bjitgroup.com>
2022-10-19 11:47:15 +0600

    added test cases for staff service

--- src/main/java/com/bjit/salon/staff/service/entity/EWorkingStatus.java
+++ src/main/java/com/bjit/salon/staff/service/entity/EWorkingStatus.java
@@ -1,7 +1,12 @@
 package com.bjit.salon.staff.service.entity;
 
 public enum EWorkingStatus {
+    INITIATED,
     PROCESSING,
+    RUNNING,
+    CANCELLED,
+    ACCEPTED,
+    ALLOCATED,
     TERMINATED,
     COMPLETED
 }
