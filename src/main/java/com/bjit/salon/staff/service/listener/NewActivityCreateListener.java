307e0e836bf65aec0f7b3a36d9413ac73f139fee
Jewel Chowdhury <jewel.chowdhury@bjitgroup.com>
2022-10-20 14:04:25 +0600

    updated the staff create activity response

--- src/main/java/com/bjit/salon/staff/service/listener/NewActivityCreateListener.java
+++ src/main/java/com/bjit/salon/staff/service/listener/NewActivityCreateListener.java
@@ -2,6 +2,7 @@ package com.bjit.salon.staff.service.listener;
 
 
 import com.bjit.salon.staff.service.dto.listener.StaffActivityCreateDto;
+import com.bjit.salon.staff.service.dto.listener.StaffActivityResponseDto;
 import com.bjit.salon.staff.service.entity.Staff;
 import com.bjit.salon.staff.service.entity.StaffActivity;
 import com.bjit.salon.staff.service.exception.StaffNotFoundException;
@@ -25,29 +26,36 @@ public class NewActivityCreateListener {
 
     private final static Logger log = LoggerFactory.getLogger(NewActivityCreateListener.class);
     private final StaffActivityRepository staffActivityRepository;
-    private final StaffRepository staffRepository;
     private final StaffMapper staffMapper;
-
 
 
     @KafkaListener(
             topics = STAFF_NEW_ACTIVITY_TOPIC,
             groupId = STAFF_NEW_ACTIVITY_GROUP
     )
-    public void newActivityCreateListener(StaffActivityCreateDto activityCreateDto) {
+    public StaffActivityResponseDto newActivityCreateListener(StaffActivityCreateDto activityCreateDto) {
         log.info("consumer: "+activityCreateDto.toString());
 
         Optional<StaffActivity> hasActivity = staffActivityRepository.
                 findByStaffIdAndReservationId(activityCreateDto.getStaffId(), activityCreateDto.getReservationId());
 
+        StaffActivity response;
         if (hasActivity.isEmpty()){
-            staffActivityRepository.save(staffMapper.toStaffActivity(activityCreateDto));
+             response = staffActivityRepository.save(staffMapper.toStaffActivity(activityCreateDto));
         }else{
             hasActivity.get().setWorkingStatus(activityCreateDto.getWorkingStatus());
             hasActivity.get().setReservationId(activityCreateDto.getReservationId());
             hasActivity.get().setStaffId(activityCreateDto.getStaffId());
-            staffActivityRepository.save(hasActivity.get());
+            response = staffActivityRepository.save(hasActivity.get());
         }
-
+        return StaffActivityResponseDto.builder()
+                .staffId(response.getStaffId())
+                .consumerId(response.getConsumerId())
+                .startTime(response.getStartTime())
+                .endTime(response.getEndTime())
+                .reservationId(response.getReservationId())
+                .workingStatus(response.getWorkingStatus())
+                .workingDate(response.getWorkingDate())
+                .build();
     }
 }
