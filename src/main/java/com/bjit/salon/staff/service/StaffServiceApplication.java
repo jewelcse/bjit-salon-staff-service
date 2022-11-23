32da1c16b96ea07d3734c010155900c015856301
jewelcse <jewelcse045@gmail.com>
2022-11-23 11:19:26 +0600

    test with zenkins

--- src/main/java/com/bjit/salon/staff/service/StaffServiceApplication.java
+++ src/main/java/com/bjit/salon/staff/service/StaffServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StaffServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StaffServiceApplication.class, args);
+		SpringApplication.run(StaffServiceApplication.class, args)
 	}
 
 }
